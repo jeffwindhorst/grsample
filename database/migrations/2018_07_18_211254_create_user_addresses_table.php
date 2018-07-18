464c1c1245031ef46ced47a3076f301267de2c69
Jeff <jeffwidhorst@gmail.com>
2018-07-18 17:30:53 -0400

    Added basic email classes with configuration for mailtra.io. Created user_profiles and user_addresses migrations.

---
+++ database/migrations/2018_07_18_211254_create_user_addresses_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_addresses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_profile_id');
+            $table->text('street');
+            $table->text('city');
+            $table->text('state');
+            $table->char('zip', 5);
+            $table->decimal('longitude', 11, 8);
+            $table->decimal('latitude', 11, 8);
+            $table->timestamps();
+
+            $table->foreign('user_profile_id')->references('id')->on('user_profiles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_addresses');
+    }
+}
